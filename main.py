02bad1966d2d0d3ced9807b06f7f5fb9642abb17
karakewe <149710285+karakewe@users.noreply.github.com>
2025-12-03 17:11:59 -0600

    Add files via Upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, File
+from extractor import extract_raw, clean_docling, add_chunks
+import uuid
+
+app = FastAPI()
+
+
+@app.get("/")
+def check():
+    return {"status": "ok"}
+
+
+@app.post("/process")
+async def process_pdf(file: UploadFile = File(...)):
+    # Guardamos el PDF temporalmente
+    file_id = str(uuid.uuid4())
+    temp_path = f"temp_{file_id}.pdf"
+
+    with open(temp_path, "wb") as f:
+        f.write(await file.read())
+
+    # Procesamiento
+    raw = extract_raw(temp_path)
+    clean = clean_docling(raw)
+    final = add_chunks(clean)
+
+    return final
